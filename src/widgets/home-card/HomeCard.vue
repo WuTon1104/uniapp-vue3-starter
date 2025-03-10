0dfa9f1ea7149320754a34bb070578076f955de2
Wu_Ton <1919567865@qq.com>
2025-03-10 10:46:20 +0800

    优化部分样式

--- src/widgets/home-card/HomeCard.vue
+++ src/widgets/home-card/HomeCard.vue
@@ -56,7 +56,7 @@ function assess(title) {
         >
           <text>{{ type }}</text>
         </view>
-        <view class="font-medium" style="margin-left: 20rpx">
+          <view style="margin-left: 20rpx">
           {{ title }}
         </view>
       </view>
